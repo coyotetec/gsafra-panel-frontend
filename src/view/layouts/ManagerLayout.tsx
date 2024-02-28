7fd1628c2db61c703f3bee9a1b0d669e3ec41eef
Iran Adryan <contatoiranadryan@gmail.com>
2024-02-28 16:00:44 -0300

    feat: add list pages

--- src/view/layouts/ManagerLayout.tsx
+++ src/view/layouts/ManagerLayout.tsx
@@ -4,9 +4,12 @@ import {
   IconLogout2,
   IconUser,
   Icon,
+  IconPlus,
 } from '@tabler/icons-react';
 import logoHorizontal from '../../assets/images/logo-horizontal.svg';
 import { NavLink, Outlet } from 'react-router-dom';
+import { cn } from '../../app/utils/cn';
+import { Button } from '../components/Button';
 
 interface NavItem {
   Icon: Icon;
@@ -35,7 +38,7 @@ export function ManagerLayout() {
 
   return (
     <div className="bg-gray-400">
-      <header className="fixed left-0 top-0 z-40 flex h-28 w-full items-center justify-center bg-white">
+      <header className="px-18 fixed left-0 top-0 z-40 flex h-28 w-full items-center justify-center bg-white">
         <div className="flex w-full max-w-8xl items-center justify-between">
           <div className="flex items-center gap-2">
             <img src={logoHorizontal} alt="logo" />
@@ -45,22 +48,25 @@ export function ManagerLayout() {
           </div>
           <div className="flex items-center gap-4">
             <strong className="text-black-80 font-semibold">Iran Adryan</strong>
-            <button className="border-primary-950 text-primary-950 flex h-13 items-center justify-center gap-2 rounded-full border-2 bg-[rgba(3,78,44,0.1)] px-6 font-semibold leading-none transition-colors hover:bg-[rgba(3,78,44,0.25)]">
+            <Button variant="outline" className="w-28">
               <IconLogout2 />
               Sair
-            </button>
+            </Button>
           </div>
         </div>
       </header>
       <aside className="h-calc-sidebar fixed left-0 top-28 z-40 w-80">
-        <div className="bg-primary-500 h-full overflow-y-auto px-3 py-4">
+        <nav className="bg-primary-500 h-full overflow-y-auto px-3 py-4">
           <ul className="space-y-2 font-semibold">
             {navItems.map(({ Icon, label, path }) => (
               <li key={label}>
                 <NavLink
                   to={path}
                   className={({ isActive }) =>
-                    `text-primary-50 hover:bg-primary-400 flex items-center gap-2 rounded-xl p-4 leading-none transition-colors ${isActive ? '!bg-primary-300 text-white' : ''}`
+                    cn(
+                      'text-primary-50 hover:bg-primary-400 flex items-center gap-2 rounded-xl p-4 leading-none transition-colors',
+                      isActive && '!bg-primary-300 text-white',
+                    )
                   }
                 >
                   <Icon />
@@ -69,7 +75,11 @@ export function ManagerLayout() {
               </li>
             ))}
           </ul>
-        </div>
+          <Button variant="light">
+            <IconPlus />
+            Nova Empresa
+          </Button>
+        </nav>
       </aside>
       <main className="min-h-calc-main ml-80 mt-28 px-9 py-10">
         <div className="mx-auto w-full max-w-8xl">
