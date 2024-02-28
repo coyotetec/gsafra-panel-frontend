bd3cfb0c5223181107dbf1f9b4c7ca8db378a0d7
Caleb Lima <lima.cardoso.caleb@gmail.com>
2024-02-28 16:19:41 -0300

    feat: componentizes Header

--- src/view/components/Header.tsx
+++ src/view/components/Header.tsx
@@ -1,10 +1,30 @@
-import logo from '../../assets/images/logo-horizontal.svg';
+import { IconLogout2 } from '@tabler/icons-react';
+import logoHorizontal from '../../assets/images/logo-horizontal.svg';
+import { Button } from './Button';
 
-export function Header() {
+interface HeaderProps {
+  isManager?: boolean;
+}
+
+export function Header({ isManager }: HeaderProps) {
   return (
-    <header className="h-32 border-2 border-black bg-white">
-      <div className="mx-auto my-auto max-w-8xl">
-        <img src={logo} alt="Logo Gsafra Horizontal" />
+    <header className="px-18 fixed left-0 top-0 z-40 flex h-28 w-full items-center justify-center border-[1px] border-black bg-white">
+      <div className="flex w-full max-w-8xl items-center justify-between">
+        <div className="flex items-center gap-2">
+          <img src={logoHorizontal} alt="logo" />
+          {isManager && (
+            <span className="rounded-full bg-primary-300 px-3 py-2 text-sm font-bold leading-none text-primary-50">
+              MANAGER
+            </span>
+          )}
+        </div>
+        <div className="flex items-center gap-4">
+          <strong className="font-semibold text-black-80">Iran Adryan</strong>
+          <Button variant="outline" className="w-28">
+            <IconLogout2 />
+            Sair
+          </Button>
+        </div>
       </div>
     </header>
   );
--- src/view/layouts/ManagerLayout.tsx
+++ src/view/layouts/ManagerLayout.tsx
@@ -1,15 +1,14 @@
 import {
   IconBell,
   IconBuildingCommunity,
-  IconLogout2,
   IconUser,
   Icon,
   IconPlus,
 } from '@tabler/icons-react';
-import logoHorizontal from '../../assets/images/logo-horizontal.svg';
 import { NavLink, Outlet } from 'react-router-dom';
 import { cn } from '../../app/utils/cn';
 import { Button } from '../components/Button';
+import { Header } from '../components/Header';
 
 interface NavItem {
   Icon: Icon;
@@ -38,23 +37,7 @@ export function ManagerLayout() {
 
   return (
     <div className="bg-gray-400">
-      <header className="px-18 fixed left-0 top-0 z-40 flex h-28 w-full items-center justify-center bg-white">
-        <div className="flex w-full max-w-8xl items-center justify-between">
-          <div className="flex items-center gap-2">
-            <img src={logoHorizontal} alt="logo" />
-            <span className="rounded-full bg-primary-300 px-3 py-2 text-sm font-bold leading-none text-primary-50">
-              MANAGER
-            </span>
-          </div>
-          <div className="flex items-center gap-4">
-            <strong className="font-semibold text-black-80">Iran Adryan</strong>
-            <Button variant="outline" className="w-28">
-              <IconLogout2 />
-              Sair
-            </Button>
-          </div>
-        </div>
-      </header>
+      <Header isManager={true} />
       <aside className="fixed left-0 top-28 z-40 h-calc-sidebar w-80">
         <nav className="h-full overflow-y-auto bg-primary-500 px-3 py-4">
           <ul className="space-y-2 font-semibold">
