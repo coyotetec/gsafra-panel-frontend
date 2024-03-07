20327a43e3bcb2ae2924997877df168087b1aba0
Caleb Lima <lima.cardoso.caleb@gmail.com>
2024-03-07 16:10:49 -0300

    feat(AuthGuard): redirect user by role

--- src/Router/AuthGuard.tsx
+++ src/Router/AuthGuard.tsx
@@ -6,10 +6,18 @@ interface AuthGuardProps {
 }
 
 export default function AuthGuard({ isPrivate }: AuthGuardProps) {
-  const { signedIn } = useAuth();
+  const { signedIn, user } = useAuth();
 
-  if (signedIn && !isPrivate) {
+  if (
+    signedIn &&
+    !isPrivate &&
+    (user?.role === 'USER' || user?.role === 'ADMIN')
+  ) {
     return <Navigate to="/" replace />;
+  }
+
+  if (signedIn && !isPrivate && user?.role === 'MANAGER') {
+    return <Navigate to="/manager/companies" replace />;
   }
 
   if (!signedIn && isPrivate) {
