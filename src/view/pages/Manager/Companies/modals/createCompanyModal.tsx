f2932fcf7fed33505b44769695f54db88306fccd
Iran Adryan <contatoiranadryan@gmail.com>
2024-03-25 11:05:01 -0300

    feat: create users after create company

--- src/view/pages/Manager/Companies/modals/createCompanyModal.tsx
+++ src/view/pages/Manager/Companies/modals/createCompanyModal.tsx
@@ -7,8 +7,12 @@ import {
 } from '../../../../../types/company';
 import { CompanyService } from '../../../../../app/services/CompanyService';
 import { CompanyForm, ICompanyFormRef } from '../components/CompanyForm';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { IconPlus } from '@tabler/icons-react';
+import { CreateUsersForm } from '../components/CreateUsersForm';
+import { GsafraUserService } from '../../../../../app/services/GsafraUserService';
+import { IGetGsafraUserResponse } from '../../../../../types/gsafraUser';
+import { Button } from '../../../../components/Button';
 
 interface CreateCompanyModalProps {
   visible: boolean;
@@ -21,16 +25,51 @@ export function CreateCompanyModal({
   onClose,
   onCreated,
 }: CreateCompanyModalProps) {
+  const [currentStep, setCurrentStep] = useState(0);
+  const [gsafraUsers, setGsafraUsers] = useState<IGetGsafraUserResponse[]>([]);
+  const [company, setCompany] = useState<IGetCompanyReponse | null>(null);
   const companyFormRef = useRef<ICompanyFormRef>(null);
+  const steps = [
+    {
+      title: 'Nova Empresa',
+      description: 'Adicione uma nova empresa ao portal.',
+    },
+    {
+      title: 'Encontramos Usuários',
+      description:
+        'Esses são os usuários que já estão criados no sistema selecione os que deseja adicionar ao portal.',
+    },
+    {
+      title: 'Usuários Criados!',
+      description:
+        'Oriente seus usuários a entrarem em seus e-mails cadastrados para finalizar o cadastro no portal.',
+    },
+  ];
+
+  async function loadGsafraUsers(companyId: string) {
+    const gsafraUsersData = await GsafraUserService.getGsafraUsers(companyId);
+
+    console.log(gsafraUsersData);
+    setGsafraUsers(gsafraUsersData);
+
+    return gsafraUsersData.length;
+  }
 
   async function handleSubmit(payload: ICompanyPayload) {
     try {
       const companyCreated = await CompanyService.createCompany(payload);
 
       onCreated({ ...companyCreated, usersQty: 0 });
-      onClose();
+      setCompany({ ...companyCreated, usersQty: 0 });
       toast.success('Empresa criada com sucesso!');
-      companyFormRef.current?.resetFields();
+
+      const gsafraUsersQty = await loadGsafraUsers(companyCreated.id);
+
+      if (gsafraUsersQty === 0) {
+        onClose();
+      } else {
+        setCurrentStep(1);
+      }
     } catch (err) {
       if (err instanceof APIError) {
         toast.error(err.message);
@@ -42,16 +81,37 @@ export function CreateCompanyModal({
     <Modal
       visible={visible}
       onClose={onClose}
-      title="Nova Empresa"
-      description="Adicione uma nova empresa ao portal."
+      title={steps[currentStep].title}
+      description={steps[currentStep].description}
+      afterClose={() => {
+        setCurrentStep(0);
+        setCompany(null);
+        setGsafraUsers([]);
+        companyFormRef.current?.resetFields();
+      }}
     >
-      <CompanyForm
-        ref={companyFormRef}
-        onClose={onClose}
-        onSubmit={handleSubmit}
-        SubmitButtonIcon={IconPlus}
-        submitButtonLabel="Criar"
-      />
+      {currentStep === 0 && (
+        <CompanyForm
+          ref={companyFormRef}
+          onClose={onClose}
+          onSubmit={handleSubmit}
+          SubmitButtonIcon={IconPlus}
+          submitButtonLabel="Criar"
+        />
+      )}
+      {currentStep === 1 && (
+        <CreateUsersForm
+          users={gsafraUsers}
+          onClose={onClose}
+          company={company}
+          goNextStep={() => setCurrentStep(2)}
+        />
+      )}
+      {currentStep === 2 && (
+        <Button className=" mx-auto mt-6 w-40" onClick={onClose}>
+          Concluir
+        </Button>
+      )}
     </Modal>
   );
 }
