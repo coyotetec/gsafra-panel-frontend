d1d145f0ac827d9c47b545cb576bd0705bb6a6b8
Iran Adryan <contatoiranadryan@gmail.com>
2024-03-25 10:51:33 -0300

    feat: create manager context

---
+++ src/app/contexts/ManagerContext.tsx
@@ -0,0 +1,61 @@
+import {
+  Dispatch,
+  MutableRefObject,
+  ReactNode,
+  SetStateAction,
+  createContext,
+  useRef,
+  useState,
+} from 'react';
+import { IGetCompanyReponse } from '../../types/company';
+import { IGetUserResponse } from '../../types/users';
+import { IGetNotificationResponse } from '../../types/notification';
+
+interface ManagerProviderProps {
+  children: ReactNode;
+}
+
+interface IManagerContextValue {
+  companies: IGetCompanyReponse[];
+  setCompanies: Dispatch<SetStateAction<IGetCompanyReponse[]>>;
+  companiesLoaded: MutableRefObject<boolean>;
+
+  users: IGetUserResponse[];
+  setUsers: Dispatch<SetStateAction<IGetUserResponse[]>>;
+  usersLoaded: MutableRefObject<boolean>;
+
+  notifications: IGetNotificationResponse[];
+  setNotifications: Dispatch<SetStateAction<IGetNotificationResponse[]>>;
+  notificationsLoaded: MutableRefObject<boolean>;
+}
+
+export const ManagerContext = createContext({} as IManagerContextValue);
+
+export function ManagerProvider({ children }: ManagerProviderProps) {
+  const [companies, setCompanies] = useState<IGetCompanyReponse[]>([]);
+  const companiesLoaded = useRef<boolean>(false);
+  const [users, setUsers] = useState<IGetUserResponse[]>([]);
+  const usersLoaded = useRef<boolean>(false);
+  const [notifications, setNotifications] = useState<
+    IGetNotificationResponse[]
+  >([]);
+  const notificationsLoaded = useRef<boolean>(false);
+
+  const value: IManagerContextValue = {
+    companies,
+    setCompanies,
+    companiesLoaded,
+
+    users,
+    setUsers,
+    usersLoaded,
+
+    notifications,
+    setNotifications,
+    notificationsLoaded,
+  };
+
+  return (
+    <ManagerContext.Provider value={value}>{children}</ManagerContext.Provider>
+  );
+}
