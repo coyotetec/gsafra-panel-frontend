200788507b541b7bd4dee625dc277f02174ae2b8
Caleb Lima <lima.cardoso.caleb@gmail.com>
2024-03-26 16:04:50 -0300

    feat(Panel): renderizes userCompanies

--- src/view/layouts/UserLaytout.tsx
+++ src/view/layouts/UserLaytout.tsx
@@ -3,7 +3,7 @@ import { Header } from '../components/Header';
 import { useState } from 'react';
 import { Sidebar } from '../pages/User/Sidebar';
 import { ContactWidget } from '../pages/User/ContactWidget';
-import { SidebarProvider } from '../../app/contexts/SidebarContext';
+import { PanelProvider } from '../../app/contexts/PanelContext';
 
 export type CurrentContentType = 'users' | 'notifications';
 
@@ -14,19 +14,20 @@ export function UserLayout() {
   return (
     <div className="bg-gray-400">
       <Header />
-      <SidebarProvider>
+
+      <PanelProvider>
         <Sidebar
           currentContent={currentContent}
           setCurrentContent={setCurrentContent}
         />
-      </SidebarProvider>
-      <main className="relative ml-96 mt-28 min-h-calc-main px-9 py-10">
-        <section className="mx-auto w-full max-w-8xl">
-          <Outlet />
-        </section>
+        <main className="relative ml-96 mt-28 min-h-calc-main px-9 py-10">
+          <section className="mx-auto w-full max-w-8xl">
+            <Outlet />
+          </section>
 
-        <ContactWidget />
-      </main>
+          <ContactWidget />
+        </main>
+      </PanelProvider>
     </div>
   );
 }
