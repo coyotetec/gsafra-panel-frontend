eccd9e64411357ec1f200158eef83266b3fa6562
Caleb Lima <lima.cardoso.caleb@gmail.com>
2024-03-26 16:31:59 -0300

    feat(Notifications): show mesage when don't exists notifications

--- src/view/pages/User/Sidebar/content/Notifications/index.tsx
+++ src/view/pages/User/Sidebar/content/Notifications/index.tsx
@@ -8,6 +8,11 @@ export function Notifications() {
     <>
       <h1 className="text-2xl font-semibold text-white">Notificações</h1>
       <div className="mt-6 flex flex-col gap-3">
+        {(notifications?.length === 0 || !notifications) && (
+          <p className="text-sm text-gray-400">
+            Não há notificações para exibir
+          </p>
+        )}
         {notifications?.map(({ id, title, body, createdAt }) => (
           <Notification key={id} title={title} body={body} date={createdAt} />
         ))}
