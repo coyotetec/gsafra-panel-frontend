6f92bae7e6030a7d6ae483870cc5cf224785b778
Caleb Lima <lima.cardoso.caleb@gmail.com>
2024-03-28 10:44:43 -0300

    fix: adds whatsapp links, changes updateUserModal title and adjusts the notifications skeleton

--- src/view/pages/User/ContactWidget.tsx
+++ src/view/pages/User/ContactWidget.tsx
@@ -15,36 +15,48 @@ export function ContactWidget() {
       >
         <img src={whatsappIcon} alt="Ícone do Whatsapp" className="w-8" />
       </button>
-      <Transition
-        appear
-        show={visible}
-        enter="ease-out duration-500"
-        enterFrom="opacity-0 translate-y-10"
-        enterTo="opacity-100  translate-y-0"
-        leave="ease-in duration-300"
-        leaveFrom="opacity-100 translate-y-0"
-        leaveTo="opacity-0 translate-y-10"
-        as={Fragment}
-      >
-        <Dialog
-          onClose={() => setVisible(false)}
-          className="fixed bottom-9 right-9 h-56 w-60 overflow-hidden rounded-xl bg-white"
+      <Transition appear show={visible} as={Fragment}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-500"
+          enterFrom="opacity-0 translate-y-10"
+          enterTo="opacity-100  translate-y-0"
+          leave="ease-in duration-300"
+          leaveFrom="opacity-100 translate-y-0"
+          leaveTo="opacity-0 translate-y-10"
         >
-          <Dialog.Panel className="relative flex flex-col gap-3 px-3 pt-9">
-            <div className="absolute left-0 right-0 top-0 flex h-9 justify-end bg-primary-900">
-              <button className="mr-2" onClick={() => setVisible(false)}>
-                <IconX size={24} stroke={2} className="text-white" />
-              </button>
-            </div>
-            <Dialog.Title className="mt-3 text-center font-semibold">
-              Que tipo de contato você necessita?
-            </Dialog.Title>
-            <div className="flex flex-col gap-2">
-              <Button className="h-12">Comercial</Button>
-              <Button className="h-12">Suporte</Button>
-            </div>
-          </Dialog.Panel>
-        </Dialog>
+          <Dialog
+            onClose={() => setVisible(false)}
+            className="fixed bottom-9 right-9 h-56 w-60 overflow-hidden rounded-xl bg-white"
+          >
+            <Dialog.Panel className="relative flex flex-col gap-3 px-3 pt-9">
+              <div className="absolute left-0 right-0 top-0 flex h-9 justify-end bg-primary-900">
+                <button className="mr-2" onClick={() => setVisible(false)}>
+                  <IconX size={24} stroke={2} className="text-white" />
+                </button>
+              </div>
+              <Dialog.Title className="mt-3 text-center font-semibold">
+                Que tipo de contato você necessita?
+              </Dialog.Title>
+              <div className="flex flex-col gap-2">
+                <a
+                  href="https://api.whatsapp.com/send?phone=559181234541"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Button className="h-12">Comercial</Button>
+                </a>
+                <a
+                  href="https://wa.me/message/T7DLAUPEFY4LH1"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Button className="h-12">Suporte</Button>
+                </a>
+              </div>
+            </Dialog.Panel>
+          </Dialog>
+        </Transition.Child>
       </Transition>
     </>
   );
