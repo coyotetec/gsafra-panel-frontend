9d8dcd5ede1f8da0972c27826c59bb8f2863274e
raul.souza <raulcesar54@hotmail.com>
2025-04-12 08:30:56 -0400

    feat

--- src/app/services/utils/api.ts
+++ src/app/services/utils/api.ts
@@ -3,15 +3,9 @@ import { localStorageKeys } from "../../config/localStorageKeys";
 import { APIError } from "../../errors/APIError";
 
 export const api = axios.create({
-  baseURL: '/api', // Using proxy path
+ baseURL: '/api', // Prod baseURL
 //  baseURL: "92.246.130.170", // Iran baseURL
 //  baseURL: "http://localhost:3003", // Iran baseURL
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  httpsAgent: {
-    rejectUnauthorized: false
-  }
 });
 
 api.interceptors.request.use(function (config) {
--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: true,
     proxy: {
       '/api': {
         target: 'http://api.painel.gsafra.com',
         changeOrigin: true,
         secure: false,
-      },
-    },
-  },
+        rewrite: path => path.replace(/^\/api/, '')
+      }
+    }
+  }
 });
